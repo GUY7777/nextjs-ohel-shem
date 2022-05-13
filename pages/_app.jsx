edc4b971cea78e98420d9901c3e1e9f938fb2c77
GUY7777 <guybabu777@gmail.com>
2022-05-13 17:24:23 +0300

    ios web height fix

--- pages/_app.jsx
+++ pages/_app.jsx
@@ -7,17 +7,19 @@ import Navbar from "../components/Navbar/Navbar";
 
 function MyApp({ Component, pageProps }) {
   return (
-    <div style={{ overflow: "hidden" }}>
+    <div>
       <Head>
         <title>אוהל-שם כדורגל איכותי בשישי</title>
         <meta name="description" content="אוהל-שם כדורגל איכותי בשישי" />
         <link rel="icon" href="/favicon.ico" />
       </Head>
       <Navbar />
-      <main style={{ minHeight: "100vh" }}>
-        <Component {...pageProps} />
-      </main>
-      <Footer />
+      <div style={{ overflow: "hidden" }}>
+        <main style={{ minHeight: "100vh" }}>
+          <Component {...pageProps} />
+        </main>
+        <Footer />
+      </div>
     </div>
   );
 }
